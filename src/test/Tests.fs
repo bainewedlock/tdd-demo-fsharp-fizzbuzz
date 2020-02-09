d854df00d0b709a9d364a77e6dd2b185c90874ca
Baine Wedlock <bainewedlock@gmail.com>
2020-02-09 08:22:51 +0100

    fscheck

--- src/test/Tests.fs
+++ src/test/Tests.fs
@@ -3,6 +3,9 @@ module Tests
 open System
 open Xunit
 open Swensen.Unquote
+open FsCheck
+open FsCheck.Xunit
+
 
 [<Fact>]
 let ``My test`` () =
@@ -15,3 +18,12 @@ let ``convertNumber returns the number if not divisible by 3 or 5`` () =
 [<Fact>]
 let ``convertNumber returns Fizz if number is divisible by 3`` () =
     "Fizz" =! Program.convertNumber 3
+
+[<Fact>]
+let fstest_demo_1 () =
+    let revRevIsOrig (xs:list<int>) = List.rev(List.rev xs) = xs
+    Check.Quick revRevIsOrig
+
+[<Property>]
+let fstest_demo_2 (xs:list<int>) =
+    List.rev(List.rev xs) = xs
